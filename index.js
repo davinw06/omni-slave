ec573c9b98806e145ffc3c186043388348b0166d
davinw06 <144931594+davinw06@users.noreply.github.com>
2025-08-12 00:01:17 -0600

    tweaks to welcome image

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ client.on('guildMemberAdd', async member => {
     const loggingChannel = member.guild.channels.cache.get(LOGGING_CHANNEL_ID);
     if (!welcomeChannel) return;
     let welcomeEmbed = new EmbedBuilder()
-        .setImage('https://i.imgur.com/KQxfKhA_d.png');
+        .setImage('https://i.imgur.com/KQxfKhA.png');
     await welcomeChannel.send({embeds: [welcomeEmbed]});
     await welcomeChannel.send({content: `Welcome <@${member.id}> to **${member.guild.name}**, we hope you enjoy your stay! Go to <#${GENERAL_CHANNEL_ID}> to chat with others or <#${INTRODUCTION_CHANNEL_ID}> to introduce yourself to everyone. Read up on <#${CHAT_CLIPS_CHANNEL_ID}> to learn more about our server. Check out <#${REACTION_ROLES_CHANNEL_ID}> to change your colors or change your roles.`});
     if (!loggingChannel) return;
